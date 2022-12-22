0cbfd20c1cbfac12662b7232bde0a248283f8251
L-V-Dragon <viotti37@gmail.com>
2022-12-22 15:46:03 +0100

    Milestone 0 Completata

---
+++ csharp-ef-players/Player.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    [Table("Player")]
+    [Index(nameof(Id), IsUnique = true)]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int score { get; set; }
+        public int GamePlayed { get; set; }
+        public int GameWon { get; set; }
+    }
+}
---
+++ csharp-ef-players/PlayerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    public class PlayerContext : DbContext
+    {
+        public DbSet<Player> Players  { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=EFPlayersDB;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
